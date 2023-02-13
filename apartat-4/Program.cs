5337e7cc0e340091bb446a9688cfac8beba01041
notbtc2 <bieloliver2006@gmail.com>
2023-02-13 12:00:41 +0100

    Updates

--- apartat-4/Program.cs
+++ apartat-4/Program.cs
@@ -5,11 +5,15 @@
         static void Main()
         {
             string? input;
+            // Cream un array de 10 edats
             int[] edats = new int[10];
+            
+            // Demanam les edats 10 pics
             for (int i = 0; i < edats.Length; i++)
             {
                 System.Console.Write($"Introdueix l\'edat {i + 1}: ");
                 input = System.Console.ReadLine();
+                // Revisam que el que ha introduit l'usuari sigui un número
                 if (input != null)
                 {
                     try
@@ -24,14 +28,16 @@
                 else i--;
             }
 
+            // Ordenam les edats
             Array.Sort(edats);
+
+            // Feim el total de les edats
+            int total = edats.Sum();
             
-            int total = 0;
-            for (int j = 0; j < edats.Length; j++)
-            {
-                total = total + edats[j];
-            }
+            // Feim la mitjana de les edats
             Console.WriteLine($"La mitjana d\'edat és {total / edats.Length}");
+            
+            // Mostram les edats més gran i més petita
             Console.WriteLine($"La edat més gran és {edats[edats.Length - 1]}");
             Console.WriteLine($"La edat més petita és {edats[0]}");
         }
